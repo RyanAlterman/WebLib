a894880d9d2987558dd6040d494d6966e46fb61f
RyanAlterman <38821584+RyanAlterman@users.noreply.github.com>
2024-03-14 22:22:40 -0700

    Project Setup

---
+++ include/WebLib.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file WebLib.hpp
+ * @author Ryan Alterman
+ * @brief
+ * @version 0.1
+ * @date 2024-03-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#include <stdio.h>
---
+++ src/WebLib.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file WebLib.cpp
+ * @author Ryan Alterman
+ * @brief
+ * @version 0.1
+ * @date 2024-03-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "WebLib.hpp"
